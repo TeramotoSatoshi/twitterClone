4d0a0f784e55116ac9dafde045eaf5ce8d491e41
寺本諭史 <fashsato11@gmail.com>
2023-09-24 17:13:12 +0900

    接続エラー修正

--- database.js
+++ database.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose"); //mongoose読み込み
 
 // Databaseクラス
 class Database {
-    constructor() {
-        this.connect();
-    }
+  constructor() {
+    this.connect();
+  }
 
-    connect() {
-        mongoose
-            .connect("mongodb://user:twittercloneabc@ac-jgjvroc-shard-00-00.xtefkpk.mongodb.net:27017,ac-jgjvroc-shard-00-01.xtefkpk.mongodb.net:27017,ac-jgjvroc-shard-00-02.xtefkpk.mongodb.net:27017/?ssl=true&replicaSet=atlas-oh8ec0-shard-0&authSource=admin&retryWrites=true&w=majority", {useNewUrlParser: true})
-            .then(() => {
-                // 正常の場合
-            })
-            .catch((err) => {
-                // エラーの場合
-                console.log("database connection error" + err);
-            });
-    }
+  connect() {
+    mongoose
+      .connect("mongodb://user:twittercloneabc@ac-jgjvroc-shard-00-00.xtefkpk.mongodb.net:27017,ac-jgjvroc-shard-00-01.xtefkpk.mongodb.net:27017,ac-jgjvroc-shard-00-02.xtefkpk.mongodb.net:27017/?ssl=true&replicaSet=atlas-oh8ec0-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        // 正常の場合
+      })
+      .catch((err) => {
+        // エラーの場合
+        console.log("database connection error" + err);
+      });
+  }
 }
 
 module.exports = new Database();
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -14,43 +14,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // GETメソッド
 router.get("/", (req, res, next) => {
-    // ページのステータスとレンダリングを行う
-    res.status(200).render("login");
+  // ページのステータスとレンダリングを行う
+  res.status(200).render("login");
 });
 
 // POSTメソッド
 router.post("/", async (req, res, next) => {
-    let payload = req.body;
-    if (req.body.logUsername && req.body.logPassword) {
-        // すべて入力されている場合
-        let user = await User.findOne({
-            // ユーザー名またはメールアドレスが登録されているか確認
-            $or: [{ username: req.body.logUsername }, { email: req.body.logUsername }],
-        }).catch(error => {
-            // 既に登録されている場合はエラー
-            console.log(error);
-            payload.errorMessage = "入力内容を確認してください";
-            res.status(200).render("login", payload);
-        });
+  let payload = req.body;
+  if (req.body.logUsername && req.body.logPassword) {
+    // すべて入力されている場合
+    let user = await User.findOne({
+      // ユーザー名またはメールアドレスが登録されているか確認
+      $or: [{ username: req.body.logUsername }, { email: req.body.logUsername }],
+    }).catch((error) => {
+      // 既に登録されている場合はエラー
+      console.log(error);
+      payload.errorMessage = "入力内容を確認してください";
+      res.status(200).render("login", payload);
+    });
 
-        if (user != null) {
-            // ユーザー情報が取得できた場合
-            // 入力されたパスワードが一致するか判別
-            let result = await bcrypt.compare(req.body.logPassword, user.password);
+    if (user != null) {
+      // ユーザー情報が取得できた場合
+      // 入力されたパスワードが一致するか判別
+      let result = await bcrypt.compare(req.body.logPassword, user.password);
 
-            if (result === true) {
-                // ユーザー情報取得
-                req.session.user = user;
-                return res.redirect("/");
-            }
-        }
-        // それ以外はエラー
-        payload.errorMessage = "ログイン情報が一致しません";
-        return res.status(200).render("login", payload);
+      if (result === true) {
+        // ユーザー情報取得
+        req.session.user = user;
+        return res.redirect("/");
+      }
     }
     // それ以外はエラー
-    payload.errorMessage = "有効な値を入力してください";
-    res.status(200).render("login", payload);
+    payload.errorMessage = "ログイン情報が一致しません";
+    return res.status(200).render("login", payload);
+  }
+  // それ以外はエラー
+  payload.errorMessage = "有効な値を入力してください";
+  res.status(200).render("login", payload);
 });
 
 // routerをどこでも使えるようにする
